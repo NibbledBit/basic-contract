e167177cd2a54182b5a7a04764d40e5f837c3001
Nibbles <me@nibbledbit.co.uk>
2022-02-16 18:14:57 +0000

    renamed contracts

--- contracts/BasicNFT_Priced.sol
+++ contracts/BasicNFT_Priced.sol
@@ -5,14 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, Ownable {
+contract MyToken_Priced is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("MyToken", "MTK") {}
 
-    function safeMint(address to) public {
+    uint256 mintPrice;
+
+    function safeMint() public payable {
+        require(msg.value > mintPrice, "Pay X ETH to mint.");
+
+        uint256 change = msg.value - mintPrice;
+        msg.sender.transfer(change);
+
+        address to = msg.sender;
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
